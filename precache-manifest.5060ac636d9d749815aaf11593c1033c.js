0ecbc8538b518e3d7fd4a5b298ee075dc665f27d
Ionut Alixandroae <ialixandroae@yahoo.com>
2022-02-12 15:04:30 +0100

    Updates

---
+++ precache-manifest.5060ac636d9d749815aaf11593c1033c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0fe5255ce4825bf782fc580501ced8",
+    "url": "/day-and-night/index.html"
+  },
+  {
+    "revision": "0eb655bdb914a618b66a",
+    "url": "/day-and-night/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "8f30c4af18920564a537",
+    "url": "/day-and-night/static/css/main.ffba7f6b.chunk.css"
+  },
+  {
+    "revision": "0eb655bdb914a618b66a",
+    "url": "/day-and-night/static/js/2.19842be7.chunk.js"
+  },
+  {
+    "revision": "ec2c751c7449cc673ec9b0995ffc876c",
+    "url": "/day-and-night/static/js/2.19842be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f30c4af18920564a537",
+    "url": "/day-and-night/static/js/main.8845801b.chunk.js"
+  },
+  {
+    "revision": "39cfd13250806db387ba",
+    "url": "/day-and-night/static/js/runtime-main.1349995c.js"
+  }
+]);
